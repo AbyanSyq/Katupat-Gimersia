313d41d1a738c8a730f765f896a5d566e851894b
Doodesz <158542315+Doodesz@users.noreply.github.com>
2025-11-27 00:54:11 +0700

    Feat: Phase 2 arena transition

--- Assets/Scripts/Environment/EnvironmentPhase.cs
+++ Assets/Scripts/Environment/EnvironmentPhase.cs
@@ -1,12 +1,27 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class EnvironmentPhase : MonoBehaviour
 {
+    [System.Serializable]
+    public class ParticlesList // stores what particles will be triggered when each phase starts
+    { 
+        public List<ParticleSystem> particles; 
+    }
+
+    [Header("References")]
+    [SerializeField] Animator anim;
+
+    [Header("Phase Effects List")]
+    [Tooltip("Each index element below is for each phase. In 1-based.")]
+    [SerializeField] List<ParticlesList> phaseTransitionParticlesList;
+
+    [Header("Stats/Debug")]
     [SerializeField] int currPhase;
 
     void Start()
     {
-        currPhase = 1;
+        currPhase = 0;
     }
 
     void OnEnable()
@@ -20,12 +35,22 @@ public class EnvironmentPhase : MonoBehaviour
     
     void OnPhaseChange(int phase)
     {
-        
+        anim.SetInteger("phase", phase);
+        TriggerPhaseTransitionEffects(phase);
     }
 
-    [ContextMenu("Trigger Next Environment Phase")]
+    void TriggerPhaseTransitionEffects(int phase)
+    {
+        foreach(ParticleSystem particle in phaseTransitionParticlesList[phase].particles)
+        {
+            particle.Play();
+        }
+    }
+
+    [ContextMenu("Trigger Next Environment Phase")] // for testing only
     void GoToNextEnvironmentPhase()
     {
-        OnPhaseChange(currPhase++);
+        currPhase++;
+        OnPhaseChange(currPhase);
     }
 }
